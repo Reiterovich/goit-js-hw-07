9a2b51b659e4b7edbcebf5e709a2186b4ddfca07
Reiterovich <reiterovich.andriy@gmail.com>
2023-09-08 22:37:53 +0300

    end?

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryCont = document.querySelector(".gallery");
+const galleryFun = createImg(galleryItems);
+
+galleryCont.insertAdjacentHTML("afterbegin", galleryFun);
+
+function createImg(galleryItems) {
+  const imgEl = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>`;
+    })
+    .join("");
+
+  return imgEl;
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
